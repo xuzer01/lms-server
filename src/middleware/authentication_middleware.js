79479477bdba85cec40d322219f1ff99c9d8af0d
Fikri <fikriramadan400@gmail.com>
2023-07-07 15:26:41 +0700

    Add Verify Staff when doing staff only related works

--- src/middleware/authentication_middleware.js
+++ src/middleware/authentication_middleware.js
@@ -41,7 +41,35 @@ const verifyAdmin = (req, res, next) => {
   }
 };
 
+const verifyStaff = (req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    jwt.verify(bearerToken, process.env.JWT_SECRET_KEY, async (err, data) => {
+      if (err) {
+        console.log("Dari jwt");
+
+        return res.status(403).send("Dari jwt");
+      }
+      const role = await Role.findOne({ where: { id: data.roleId } });
+      if (role === null || role.name !== "Staff") {
+        console.log("Dari validasi");
+
+        return res.status(403).send("Dari validasi");
+      }
+      if (err === null && role !== null && role.name === "Staff") {
+        req.user = data;
+        next();
+      }
+    });
+  } else {
+    return res.send(403);
+  }
+};
+
 module.exports = {
   verifyToken,
   verifyAdmin,
+  verifyStaff,
 };
